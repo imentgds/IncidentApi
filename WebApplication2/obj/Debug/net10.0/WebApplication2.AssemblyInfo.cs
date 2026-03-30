2a83c2281266f03538e49fd56cb30e14da8a65d7
hsendalila <hsendalila@gmail.com>
2026-03-30 12:16:13 +0100

    modfi incidentstatus

--- WebApplication2/obj/Debug/net10.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net10.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c450e82d9e61564fb4049094694cc16964f92dbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c1139304dff481016d17d4aea48a069de79ef13")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
