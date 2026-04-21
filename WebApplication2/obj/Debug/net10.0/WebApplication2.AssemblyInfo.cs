1376704da410d55d5b6e63abc6698cd6be7c3e38
Gueddes Imen <imengds@gmail.com>
2026-04-21 11:51:47 +0100

    m3

--- WebApplication2/obj/Debug/net10.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net10.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dfa2fff8d9d1676aba9d3f556734f7be231365a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c544a702ad45c632c7ad155f446fb49403c91a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
