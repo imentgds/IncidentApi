23702b6da4665f9e46c5502922e8d6a71aa9371d
Gueddes Imen <imengds@gmail.com>
2026-04-22 18:25:43 +0100

    Add CI pipeline for Windows

--- WebApplication2/obj/Debug/net10.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net10.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cf83ffb90a92c8d2391f35d15c398518b3576b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+898a52180f5e97489a2ce5fc154a3f7e25f9a663")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
